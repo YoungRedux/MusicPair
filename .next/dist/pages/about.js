1cc4d673bb241098bd6d3c63a325dcf7f6096c1d
Riley Adair <rileydadair@gmail.com>
2018-01-03 23:35:38 -0600

    added pages

---
+++ .next/dist/pages/about.js
@@ -0,0 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/riley/Desktop/musicPair/MusicPair/pages/about.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, _react2.default.createElement(_link2.default, { href: "/", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, "Home"), _react2.default.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, "This is the about page"));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2Fib3V0LmpzIl0sIm5hbWVzIjpbIkxpbmsiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQUE7eUJBQ2IsY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRSxBQUFDLGdDQUFLLE1BQU4sQUFBVztnQkFBWDtrQkFBQTtBQUFBO0tBREYsQUFDRSxBQUNBLHlCQUFBLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUhXLEFBQ2IsQUFFRTtBQUhKIiwiZmlsZSI6ImFib3V0LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9yaWxleS9EZXNrdG9wL211c2ljUGFpci9NdXNpY1BhaXIifQ==
---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default () => (
+  <div>
+    <Link href="/">Home</Link>
+    <p>This is the about page</p>
+  </div>
+)
